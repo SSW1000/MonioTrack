94ad7737ca8ac15c6ebc635059cb1254387490d6
Supun Sandeepa Wimalarathne <supun2019origin@gmail.com>
2023-12-17 00:49:23 +0530

    Updated mysql database file

--- sql/iet.sql
+++ sql/iet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 15, 2023 at 08:08 PM
+-- Generation Time: Dec 17, 2023 at 12:44 AM
 -- Server version: 8.0.18
 -- PHP Version: 7.3.12
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `bank_accounts` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `cash` (
   `cash_balance` decimal(15,2) DEFAULT '0.00',
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -70,6 +70,26 @@ CREATE TABLE IF NOT EXISTS `expensecategories` (
   `Description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`ExpenseCategoryID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `expensecategories`
+--
+
+INSERT INTO `expensecategories` (`ExpenseCategoryID`, `ExpenseCategoryName`, `Description`) VALUES
+(1, 'Housing', 'Expenses related to accommodation.'),
+(2, 'Utilities', 'Bills for essential services like electricity, water.'),
+(3, 'Transportation', 'Costs associated with commuting or owning a vehicle.'),
+(4, 'Food and Groceries', 'Expenses for purchasing food and household supplies.'),
+(5, 'Healthcare', 'Medical expenses, insurance, prescriptions, etc.'),
+(6, 'Debt Payments', 'Payments for loans, credit cards, or other debts.'),
+(7, 'Entertainment', 'Costs for leisure activities, subscriptions, etc.'),
+(8, 'Personal Care', 'Expenses for clothing, grooming, personal items.'),
+(9, 'Education', 'Costs related to education, tuition fees, supplies.'),
+(10, 'Savings and Investments', 'Funds set aside for savings, retirement, or investments.'),
+(11, 'Charitable Donations', 'Contributions to charitable organizations.'),
+(12, 'Taxes', 'Payments made for income, property, or sales taxes.'),
+(13, 'Insurance', 'Premiums paid for various insurance policies.'),
+(14, 'Childcare/Child Expenses', 'Costs related to childcare, education for children.');
 
 -- --------------------------------------------------------
 
@@ -91,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `expenses` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `fk_expenses_bank_accounts` (`bank_account_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -113,7 +133,7 @@ CREATE TABLE IF NOT EXISTS `income` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `fk_bank_account_id` (`bank_account_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -128,6 +148,21 @@ CREATE TABLE IF NOT EXISTS `incomecategories` (
   `Description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`IncomeCategoryID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `incomecategories`
+--
+
+INSERT INTO `incomecategories` (`IncomeCategoryID`, `IncomeCategoryName`, `Description`) VALUES
+(1, 'Salary/Wages', 'Regular income from employment.'),
+(2, 'Self-Employment Income', 'Earnings from freelance work, consulting, or business.'),
+(3, 'Interest Income', 'Money earned from interest on savings, investments, bonds.'),
+(4, 'Dividend Income', 'Income earned from owning stocks that pay dividends.'),
+(5, 'Rental Income', 'Earnings from renting out property or real estate.'),
+(6, 'Royalties', 'Income generated from licensing intellectual property.'),
+(7, 'Commission Income', 'Earnings from sales or transactions with commissions.'),
+(8, 'Pension/Retirement Income', 'Regular payments received upon retirement.'),
+(9, 'Capital Gains', 'Profit from selling an investment or asset.');
 
 -- --------------------------------------------------------
 
@@ -145,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `mobile_number` (`mobile_number`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
